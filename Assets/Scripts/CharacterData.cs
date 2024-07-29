0788b192aeaf0a542001235ca6c544bf60efb45f
NordaCelestia <fatihkocaman797@gmail.com>
2024-07-29 16:19:33 +0300

    new features for hp bars
    hp bars now following the character and has animation.

--- Assets/Scripts/CharacterData.cs
+++ Assets/Scripts/CharacterData.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 public class CharacterData : MonoBehaviour
 {
-    
     [SerializeField] private CharacterDataScriptableObject characterData;
+    [SerializeField] Image hpBar;
+    [SerializeField] GameObject healthBar;
 
+    float healthPercentage;
     private int currentHealth;
 
     private void Start()
@@ -15,20 +17,38 @@ public class CharacterData : MonoBehaviour
         currentHealth = characterData.maxHealth;
     }
 
-    
+    private void Update()
+    {
+        // healthBar GameObject'inin sadece x pozisyonunu this.gameObject'in x pozisyonuna eþitleme
+        Vector3 newPosition = healthBar.transform.position;
+        newPosition.x = this.gameObject.transform.position.x;
+        healthBar.transform.position = newPosition;
 
-    public void takeDamage()
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            TakeDamage();
+        }
+    }
+
+    public void TakeDamage()
     {
         currentHealth -= 1;
         Debug.Log(characterData.characterName + " takes damage! remaining health: " + currentHealth);
+        UpdateUI();
+    }
+
+    public void UpdateUI()
+    {
+        healthPercentage = (float)currentHealth / (float)characterData.maxHealth;
+        hpBar.fillAmount = healthPercentage;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("snowball"))
         {
-            takeDamage();
+            TakeDamage();
+            collision.gameObject.SetActive(false);
         }
     }
-
 }
